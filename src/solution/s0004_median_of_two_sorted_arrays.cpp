563e693509cbe1ff84d9bf87fe11a59e1ff8d69d
HE7086 <me@heyi7086.com>
2025-06-13 08:10:54 +0200

    s0004

--- src/solution/s0004_median_of_two_sorted_arrays.cpp
+++ src/solution/s0004_median_of_two_sorted_arrays.cpp
@@ -39,7 +39,36 @@ using namespace std;
 class Solution {
 public:
   double findMedianSortedArrays(vector<int> const& nums1, vector<int> const& nums2) {
-    return 0;
+    if (nums1.size() > nums2.size()) {
+      return findMedianSortedArrays(nums2, nums1);
+    }
+
+    int m = nums1.size(), n = nums2.size();
+    int left = 0, right = m;
+
+    while (left <= right) {
+      int partA = (left + right) / 2;
+      int partB = (m + n + 1) / 2 - partA;
+
+      int maxLeftA = (partA == 0) ? INT_MIN : nums1[partA - 1];
+      int minRightA = (partA == m) ? INT_MAX : nums1[partA];
+      int maxLeftB = (partB == 0) ? INT_MIN : nums2[partB - 1];
+      int minRightB = (partB == n) ? INT_MAX : nums2[partB];
+
+      if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
+        if ((m + n) % 2 == 0) {
+          return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2.0;
+        } else {
+          return max(maxLeftA, maxLeftB);
+        }
+      } else if (maxLeftA > minRightB) {
+        right = partA - 1;
+      } else {
+        left = partA + 1;
+      }
+    }
+
+    return 0.0;
   }
 };
 
